c358006e2d85367cc03824149a1f3ba21a082f49
Soobin <ssbin93@gmail.com>
2021-12-21 23:57:29 +1000

    ongoing

--- components/NavBar.js
+++ components/NavBar.js
@@ -3,8 +3,13 @@ import Link from "next/link";
 export default function NavBar() {
   return (
     <nav>
-      <Link href="/">Home</Link>
-      <Link href="/about">About</Link>
+      <Link href="/">
+        <a>Home</a>
+      </Link>
+
+      <Link href="/about">
+        <a>About</a>
+      </Link>
     </nav>
   );
 }
