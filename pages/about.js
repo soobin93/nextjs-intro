0e480b7d90a0c154a8a4927d123a67299fa117f5
Soobin <ssbin93@gmail.com>
2021-12-21 23:55:05 +1000

    ongoing

--- pages/about.js
+++ pages/about.js
@@ -1,3 +1,10 @@
+import NavBar from "../components/NavBar";
+
 export default function About() {
-  return 'About Us';
+  return (
+    <div>
+      <NavBar/>
+      <h1>About Us</h1>
+    </div>
+  );
 }
