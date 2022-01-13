22ea1967765bd19b40cf7875656f75c0e09d9565
Soobin <ssbin93@gmail.com>
2022-01-13 22:51:17 +1000

    Redirects & Rewrites

--- next.config.js
+++ next.config.js
@@ -1,3 +1,22 @@
+const API_KEY = process.env.API_KEY;
+
 module.exports = {
   reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: "/github",
+        destination: "https://github.com/soobin93",
+        permanent: false
+      }
+    ];
+  },
+  async rewrites() {
+    return [
+      {
+        source: "/api/movies",
+        destination: `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`
+      }
+    ];
+  }
 }
